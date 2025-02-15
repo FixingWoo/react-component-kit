9f01064ac263d4d28bbc91998f096f7df3b88da3
FixingWoo <fixingwoo@gmail.com>
2025-02-15 14:23:03 +0900

    refactor: Checkbox Prop 명 변경

--- src/components/form-fields/Checkbox/index.tsx
+++ src/components/form-fields/Checkbox/index.tsx
@@ -5,12 +5,12 @@ import styles from './style.module.scss';
 
 import Icon, { IconName } from '@/components/Icon';
 
-interface ICheckBoxProps extends React.InputHTMLAttributes<HTMLInputElement> {
+interface IProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string;
   error?: boolean;
 }
 
-const CheckBox = React.forwardRef<HTMLInputElement, ICheckBoxProps>(
+const CheckBox = React.forwardRef<HTMLInputElement, IProps>(
   ({ label, error, className, ...props }, ref) => {
     return (
       <div className={styles.checkBoxWrapper}>
@@ -30,6 +30,7 @@ const CheckBox = React.forwardRef<HTMLInputElement, ICheckBoxProps>(
               ref={ref}
               {...props}
             />
+
             <Icon className={styles.checkIcon} name={IconName.CHECK} />
           </div>
           {label}
